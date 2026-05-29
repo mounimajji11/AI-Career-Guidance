4d4840a2a82179c4a8124b779265ffa0bfa430e5
Mouni Majji <mounimajji134@gmail.com>
2026-05-29 12:07:54 +0530

    fixed render port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_file, send_from_directory, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -89,5 +90,6 @@ def predict():
         })
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
